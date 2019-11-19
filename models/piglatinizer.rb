c9a207901016f3c5b3bc989ae0765937cd992861
Huda Yousif <hjoski@gmail.com>
2019-11-19 11:51:51 -0800

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,8 +3,6 @@ class PigLatinizer
     def piglatinize(input_str)
         input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
     end
-
-    private
   
     def consonant?(char)
       !char.match(/[aAeEiIoOuU]/)
